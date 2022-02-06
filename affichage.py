c0c3f3229fa6f2e9270a70ae64e1d9e1d4122844
ProgerPickomino <progerpickomino@gmail.com>
2022-02-06 19:59:02 +0100

    ajout de la fonction qui qffiche les dominos

--- affichage.py
+++ affichage.py
@@ -1,8 +1,25 @@
 
-def AficheJoueur(liste_des_joueur):
+def AfficheJoueur(liste_des_joueur):
     for i in range(len(liste_des_joueur)):
         print(liste_des_joueur[i][0], ": ", end = "")
         if len(liste_des_joueur[i][1]) == 0:
-            print("le joueur na pas de jetons")
+            print("Le joueur n'a pas de jetons")
         else:
-            print("sons jetons le plus haut et :", liste_des_joueur[i][1][-1])
+            print("Son jetons le plus haut et :", liste_des_joueur[i][1][-1])
+
+def affichage_dominos(Liste):
+    """List --> None 
+    affiche les dominos
+    """
+    if len(Liste)==0:
+        print("Toues les dominos ont ete joues")
+    print(' -----  '*len(Liste))
+    for i in range(len(Liste)):
+        print(' |',Liste[i][0],'| ', end = '')
+    print(' ')
+    print(' -----  '*len(Liste), end='') 
+    print(' ')  
+    for i in range(len(Liste)):
+        print(' |',Liste[i][1],'|  ', end = '')  
+    print(' ')
+    print(' -----  '*len(Liste), end='') 
