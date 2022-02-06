a2253f54a35a6b471d1e3a8f53a6b0bc9793a481
ProgerPickomino <progerpickomino@gmail.com>
2022-02-06 21:03:07 +0100

    correction des noms de fonctions

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import initialisationJeu as ij
-from afichage import *
-def JeuPlusieurJourer():
+import initialisationJeu as ij  
+from affichage import *
+def JeuPlusieurJoueur():
     jetons_en_jeux = [(21, 1), (22, 1), (23, 1), (24, 1), (25, 2), (26, 2), (27, 2), (28, 2), (29, 3), (30, 3), (31, 3), (32, 3), (33, 4), (34, 4), (35, 4), (36, 4)]
+    affichage_dominos(jetons_en_jeux)
     jetons_retourner = 0
     liste_des_joueur = ij.InitJoueur()
-    AficheJoueur(liste_des_joueur)
+    AfficheJoueur(liste_des_joueur)
+    
 
-JeuPlusieurJourer()
+JeuPlusieurJoueur()
