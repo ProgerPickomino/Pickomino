a5ae17ff53c0776c458a2abb5a0d25e49c549fae
ProgerPickomino <progerpickomino@gmail.com>
2022-02-15 15:08:19 +0100

    ajout des fonctons pour afficher les dominos

--- afichage.py
+++ afichage.py
@@ -1,8 +1,54 @@
 
-def AficheJoueur(liste_des_joueur):
+def AfficheJoueur(liste_des_joueur):
     for i in range(len(liste_des_joueur)):
         print(liste_des_joueur[i][0], ": ", end = "")
         if len(liste_des_joueur[i][1]) == 0:
-            print("le joueur na pas de jetons")
+            print("Le joueur n'a pas de jetons")
         else:
-            print("sons jetons le plus haut et :", liste_des_joueur[i][1][-1])
+            print("Son jetons le plus haut et :", liste_des_joueur[i][1][-1])
+
+def affichage_dominos(Liste):
+    """List --> None 
+    affiche les dominos
+    """
+    if len(Liste)==0:
+        print("Toues les dominos ont ete joues")
+    print(' -----  '*len(Liste))
+    for i in range(len(Liste)):
+        print(' |',Liste[i][0],'| ', end = '')
+    print(' ')
+    print(' -----  '*len(Liste), end='') 
+    print(' ')  
+    for i in range(len(Liste)):
+        print(' |',Liste[i][1],'|  ', end = '')  
+    print(' ')
+    print(' -----  '*len(Liste), end='') 
+
+def affiche_des(lance_de_des):
+    list_des = []
+    """ 
+    Dic --> None
+    """
+    for des in lance_de_des :
+        for i in range(lance_de_des[des]):
+            list_des.append(des)
+    print(list_des) 
+
+def dominos_caches(list_dominos, dernier_visible = True):
+    '''
+    List x Bool --> List
+    masque tous les dominos du joueur sauf le dernier
+    '''
+    if dernier_visible:
+        nb_dominos_caches = len(list_dominos)-1
+    else :
+        nb_dominos_caches = len(list_dominos)
+    dernier_dominos = list_dominos[-1]
+    l_dominos_caches = [('# ', '#') for i in range(nb_dominos_caches)]
+    l_dominos_caches.append(dernier_dominos)
+    return l_dominos_caches
+
+affichage_dominos(dominos_caches([(29, 3), (30, 3), (31, 3), (32, 3), (33, 4), (34, 4), (35, 4), (36, 4)]))
+
+
+#fonction qui affiche les dominos retournes
