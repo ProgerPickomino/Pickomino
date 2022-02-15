2ba1925511d3bf391a4e9e3d18e93696cd6dab2c
ProgerPickomino <progerpickomino@gmail.com>
2022-02-15 15:06:10 +0100

    suppression du fichier afichage.py

--- afichage.py
+++
@@ -1,8 +0,0 @@
-
-def AficheJoueur(liste_des_joueur):
-    for i in range(len(liste_des_joueur)):
-        print(liste_des_joueur[i][0], ": ", end = "")
-        if len(liste_des_joueur[i][1]) == 0:
-            print("le joueur na pas de jetons")
-        else:
-            print("sons jetons le plus haut et :", liste_des_joueur[i][1][-1])
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import initialisationJeu as ij  
+from gameplay import *
 from affichage import *
+
 def JeuPlusieurJoueur():
     jetons_en_jeux = [(21, 1), (22, 1), (23, 1), (24, 1), (25, 2), (26, 2), (27, 2), (28, 2), (29, 3), (30, 3), (31, 3), (32, 3), (33, 4), (34, 4), (35, 4), (36, 4)]
     affichage_dominos(jetons_en_jeux)
     jetons_retourner = 0
     liste_des_joueur = ij.InitJoueur()
     AfficheJoueur(liste_des_joueur)
+
+    #lancer les des, les afficher et afficher les dominos de chaque joueur
     
 
 JeuPlusieurJoueur()
